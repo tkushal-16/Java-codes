5e3b6cd182716c77ecba45d7e7551434f97db9cb
tkushal-16 <kushaltkanaiaha7777@gmail.com>
2026-02-02 22:28:29 +0530

    Initial commit of this repo

---
+++ src/main/java/com/spring/java/basic/JavaInput.java
@@ -0,0 +1,15 @@
+package com.spring.java.basic;
+
+import java.util.Scanner;
+
+public class JavaInput {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value : ");
+
+        int n = input.nextInt();
+
+        System.out.println("Entered value is :" + n);
+    }
+}
---
+++ src/main/java/com/spring/java/basic/ReverseNum.java
@@ -0,0 +1,18 @@
+package com.spring.java.basic;
+
+public class ReverseNum {
+
+    public static void main(String[] args) {
+        int num = 121212;
+
+        int ans = 0;
+        while(num > 0){
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println("The reverse of num is: " + ans);
+    }
+}
